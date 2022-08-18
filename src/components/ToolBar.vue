9a26d9879b9428109118690d74bd247fb7b59e2d
yeongdonge <parrineau@gmail.com>
2022-08-18 18:01:58 +0900

    RouterLink를 이용한 Toolbar 구현

---
+++ src/components/ToolBar.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="header">
+    <router-link to="/news">News</router-link> |
+    <router-link to="/ask">Ask</router-link> |
+    <router-link to="/jobs">Jobs</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scpoed>
+.header {
+    color: white;
+    background-color: #42b883;
+    display: flex;
+    padding: 8px;
+}
+.header .router-link-exact-actvice {
+    color: #35495e;
+}
+.header a {
+    color: white;
+}
+body {
+    padding: 0;
+    margin: 0;
+}
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,26 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import NewsView from '../views/NewsView.vue'
-import AskView from '../views/AskView.vue'
-import JobsView from '../views/JobsView.vue'
+import NewsView from "../views/NewsView.vue";
+import AskView from "../views/AskView.vue";
+import JobsView from "../views/JobsView.vue";
 Vue.use(VueRouter);
 
 export const router = new VueRouter({
   routes: [
     {
-      path: '/news',
+      path: "/",
+      redirect: "/news",
+    },
+    {
+      path: "/news",
       component: NewsView,
     },
     {
-      path: '/ask',
+      path: "/ask",
       component: AskView,
     },
     {
-      path: '/jobs',
+      path: "/jobs",
       component: JobsView,
     },
   ],
