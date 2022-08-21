9253bb0ed8dbd32b06224b0c5d7b95739306f7e9
yeongdonge <parrineau@gmail.com>
2022-08-21 16:44:33 +0900

    트랜지션 적용

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <tool-bar></tool-bar>
-    <router-view></router-view>
+    <transition name="page">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
@@ -17,5 +19,16 @@ export default {
 <style>
 a {
   text-decoration: none;
+}
+
+/* Router Transition */
+.page-enter-active,
+.page-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
 }
 </style>
