6890bbaaa20b55654ae5f1e12284a2b455a0bbe4
yeongdonge <parrineau@gmail.com>
2022-08-24 19:16:40 +0900

    api 호출 async/await 구조 개선

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,49 +1,30 @@
 import { fetchUserInfo, fetchItemInfo, fetchList } from "../api/index.js";
 export default {
-  // FETCH_NEWS({ commit }) {
-  //   fetchNewsList()
-  //     .then(({ data }) => {
-  //       commit("SET_NEWS", data);
-  //       return data;
-  //     })
-  //     .catch((error) => console.log(error));
-  // },
-  // FETCH_JOBS({ commit }) {
-  //   fetchJobsList()
-  //     .then(({ data }) => {
-  //       commit("SET_JOBS", data);
-  //     })
-  //     .catch((error) => console.log(error));
-  // },
-  // FETCH_ASK({ commit }) {
-  //   fetchAskList()
-  //     .then(({ data }) => {
-  //       commit("SET_ASK", data);
-  //     })
-  //     .catch((error) => console.log(error));
-  // },
-  FETCH_USER( { commit }, userName) {
-    return fetchUserInfo(userName)
-    .then(({ data }) => {
-      commit('SET_USER', data);
-      return data;
-    })
-    .catch((error) => console.log(error));
+  async FETCH_USER({ commit }, userName) {
+    try {
+      const response = await fetchUserInfo(userName);
+      commit("SET_USER", response.data);
+      return response;
+    } catch (error) {
+      console.log(error);
+    }
   },
-  FETCH_ITEM( { commit }, id) {
-    return fetchItemInfo(id)
-    .then(({ data }) => {
-      commit('SET_ITEM', data);
-      return data;
-    })
-    .catch((error) => console.log(error))
+  async FETCH_ITEM({ commit }, id) {
+    try {
+      const response = await fetchItemInfo(id);
+      commit("SET_ITEM", response.data);
+      return response;
+    } catch (error) {
+      console.log(error);
+    }
   },
-  FETCH_LIST( {commit}, pageName) {
-    return fetchList(pageName)
-    .then(( {data}) => {
-      commit('SET_LIST', data);
-      return data;
-    })
-    .catch((error) => console.log(error))
+  async FETCH_LIST({ commit }, pageName) {
+    try {
+      const response = await fetchList(pageName);
+      commit("SET_LIST", response.data);
+      return response;
+    } catch (error) {
+      console.log(error);
+    }
   },
-}
+};
